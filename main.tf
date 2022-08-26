35a2847813f24e71c9e1a6b41bfdf6742079c03b
mvpklearn <mvpklearn@gmail.com>
2022-08-26 14:05:50 +0530

    First commit

---
+++ main.tf
@@ -0,0 +1,112 @@
+#test1
+provider "aws" {
+    region = var.aws-region
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+resource "aws_vpc" "prod-vpc" {
+    cidr_block = var.vpc-cidr-block
+}
+
+resource "aws_internet_gateway" "prod-igw" {
+    vpc_id = aws_vpc.prod-vpc.id
+}
+
+resource "aws_route_table" "prod-rt" {
+    vpc_id = aws_vpc.prod-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.prod-igw.id
+    }
+
+}
+resource "aws_subnet" "prod-subnet" {
+    vpc_id = aws_vpc.prod-vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = var.aws-region
+}
+
+resource "aws_route_table_association" "prod-rt-ass" {
+    subnet_id = aws_subnet.prod-subnet.id
+    route_table_id = aws_route_table.prod-rt.id
+}
+
+resource "aws_security_group" "prod-sg" {
+    name        = "allow_web"
+  description = "Allow web traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+  
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+resource "aws_network_interface" "nwif" {
+    subnet_id = aws_subnet.prod-subnet.id
+    private_ips = ["10.0.1.50"]
+    security_groups = [aws_security_group.prod-sg.id]
+}
+
+resource "aws_eip" "prod-eip" {
+    vpc = true
+    network_interface = aws_network_interface.nwif.id
+    associate_with_private_ip = "10.0.1.50"
+    depends_on = [
+      aws_internet_gateway.prod-igw
+    ]
+}
+
+resource "aws_instance" "my-prod-web-server1" {
+    ami = "ami-052efd3df9dad4825"
+    instance_type = var.instance_type
+    network_interface {
+    network_interface_id = aws_network_interface.nwif.id
+    device_index         = 0
+  }
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt-get update -y
+                sudo apt-get install -y apache2
+                sudo systemctl start apache2.service
+                sudo bash -c 'echo My very first web server--: > /var/www/html/index.html'
+                EOF
+  tags = {
+    Name = "prod-server"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,32 @@
+#test1
+variable "aws-region" {
+    description = "default AWS region:"
+    type = string
+    default = "us-east-1"
+}
+
+variable "vpc-cidr-block" {
+    description = "VPC default CIDR block"
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "instance_type" {
+    description = "Type of the default instance:"
+    type = string
+    default = "t2.micro"
+}
+
+variable "access_key" {
+    description = "access key"
+    type = string
+    sensitive = true
+  
+}
+
+variable "secret_key" {
+    description = "secret key"
+    type = string
+    sensitive = true
+  
+}
